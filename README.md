b52b19aab0b8d154b0511586c487106ff2095b71
Mc-zen <alphabetagamma11@hotmail.com>
2022-07-20 12:35:49 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,35 @@
+# Matrix for C++
+
+
+A tiny C++20 general-purpose library for dense, fixed-size matrices (and vectors) with no dependencies.
+It makes use of C++20s concepts to enable special features such as norm for vectors.
+
+## Overview
+
+Features:
+
+- intuitive syntax
+- almost no overhead
+- header-only
+- fully constexpr
+- STL-oriented
+- static memory (e.g. suited for audio development)
+- simple software design in compaison to some other matrix libs, e.g. no CRTP, virtual functions, ...
+- fully tested. Tests can be run with Catch2.
+
+Limitations:
+
+- only C++20 (and up) because of heavy use of concepts and constexpr
+- this is not a linear algebra library - at the moment the library provides no linear algebra algorithms such as decomposition and solvers (exception: determinant and inverse of 2&times;2 and 3&times;3 matrices).
+
+
+## Classes
+| Class | Definition |
+|-------|------------|
+|`Matrix<T,m,n>`   | Main class for `m`×`n` matrices of type `T` |
+|`Matrix_view<T>`   | Matrix view class for viewing or modifying parts of a matrix like a block, row or column. |
+|`Matrix_iterator<T>`   | Random access iterator for iterating through a contiguous array in memory (matrix data). |
+|`Matrix_stride_iterator<T>`   | Random access iterator for iterating i.e. vertically through a contiguous array in memory.   |
+|`Matrix_block_iterator<T>`   | Bi-directional iterator for iterating through a matrix block within a bigger matrix. |
+
+
---
+++ src/solver.h
@@ -0,0 +1,49 @@
+#pragma once
+
+// Compute determinant and inverse of 2x2 as well as 3x3 matrices. 
+
+#include "matrix.h"
+
+
+namespace algebra{
+
+template<class T, Index n>
+T det(const Matrix<T, n, n>& a);
+
+template<class T, Index n>
+Matrix<T, n, n> inv(const Matrix<T, n, n>& a);
+
+template<class T>
+T det(const Matrix<T, 2, 2>& a) {
+	const auto data = a.data();
+	return data[0] * data[3] - data[1] * data[2];
+}
+
+template<class T>
+T det(const Matrix<T, 3, 3>& a) {
+	const auto data = a.data();
+	return data[0] * (data[4] * data[8] - data[7] * data[5]) + data[1] * (data[5] * data[6] - data[3] * data[8]) + data[2] * (data[3] * data[7] - data[6] * data[4]);
+}
+template<class T>
+Matrix<T, 2, 2> inv(const Matrix<T, 2, 2>& a) {
+	const auto data = a.data();
+	Matrix<T, 2, 2> result{ data[3], -data[1], -data[2], data[0] };
+	return result /= det(a);
+}
+
+
+template<class T>
+Matrix<T, 3, 3> inv(const Matrix<T, 3, 3>& a) {
+	const auto data = a.data();
+	T eihf = data[4] * data[8] - data[7] * data[5];
+	T fgdi = data[5] * data[6] - data[3] * data[8];
+	T dhge = data[3] * data[7] - data[6] * data[4];
+	T det = data[0] * eihf + data[1] * fgdi + data[2] * dhge;
+	Matrix<T, 3, 3> result{
+		eihf,data[2] * data[7] - data[1] * data[8],data[1] * data[5] - data[2] * data[4],
+		fgdi,data[0] * data[8] - data[2] * data[6],data[3] * data[2] - data[0] * data[5],
+		dhge,data[6] * data[1] - data[0] * data[7],data[0] * data[4] - data[3] * data[1] };
+	return result *= T(1.0) / det;
+}
+
+}
